79c259d4bf612b6f065c4c0718b09a4680a9846b
mabuhanifa <moh.abuhanifa@gmail.com>
2024-02-18 18:55:58 +0600

    encryption

---
+++ index.js
@@ -0,0 +1,18 @@
+const { log } = console;
+const { generateKeyPairSync } = require("crypto");
+
+const { privateKey, publicKey } = generateKeyPairSync("rsa", {
+  modulusLength: 2048,
+  publicKeyEncoding: {
+    type: "spki",
+    format: "pem",
+  },
+  privateKeyEncoding: {
+    type: "pkcs8",
+    format: "pem",
+    cipher: "aes-256-cbc",
+    passphrase: "top secret",
+  },
+});
+
+log(privateKey, publicKey);
