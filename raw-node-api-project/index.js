f62b09a9ef2c1ac910687ee99cd285fb95d3e400
mabuhanifa <moh.abuhanifa@gmail.com>
2025-02-17 23:45:19 +0600

    raw node app created

---
+++ raw-node-api-project/index.js
@@ -0,0 +1,28 @@
+/*
+Title: UpTime Monitoring  Application
+Author: Mohammed Abu Hanifa
+Date: 2023-03-15
+Description: A RESTfull API for upTime monitoring
+*/
+
+const http = require("http");
+
+const app = {};
+
+app.config = {
+  port: 3000,
+};
+
+app.createServer = () => {
+  const server = http.createServer(app.handleReqRes);
+  server.listen(app.config.port, () => {
+    console.log(`Listening on port ${app.config.port}`);
+  });
+};
+
+app.handleReqRes = (req, res) => {
+  //   res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello World");
+};
+
+app.createServer();
